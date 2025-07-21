c4618f6d091ecd9d725f1bc104d3128b7072b121
Abrahamthefirst <abrahamodusegun36@gmail.com>
2025-07-21 03:15:26 +0100

    toggle modal fix

--- src/components/Home/EditTag.tsx
+++ src/components/Home/EditTag.tsx
@@ -14,7 +14,7 @@ const EditTag = ({ closeModal }: EditTagProps) => {
     <div className="flex flex-col items-center justify-center bg-amber-300 rounded-md md:w-1/3">
       <div>
         <h3 className="text-white">Edit Tags</h3>
-        <button onClick={() => closeModal} className='cursor-pointer'>X</button>
+        <button onClick={closeModal} className='cursor-pointer'>X</button>
       </div>
 
       <hr />
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,33 +1,25 @@
 import AnchorButton from '@/components/common/AnchorButton';
-import { useState } from 'react';
-import { useRef } from 'react';
+import { useRef} from 'react';
 import EditTag from '@/components/Home/EditTag';
 import { Button } from '@/components/ui/button';
 
 const Header = () => {
-  const createButtonClass = {
-    'bg-green-400': true,
-  };
   const dialogRef = useRef<HTMLDialogElement>(null);
-  console.log(dialogRef)
 
   return (
     <header className="my-4 flex justify-between">
       <span>Notes</span>
       <div className="flex space-x-8">
-        <AnchorButton to="new" className={createButtonClass}>
+        <AnchorButton to="new" className="bg-green-400">
           Create
         </AnchorButton>
 
-        <Button
-          className="cursor-pointer"
-          onClick={() => dialogRef.current?.showModal()}
-        >
+        <Button className="cursor-pointer" onClick={() => dialogRef.current?.showModal()}>
           Edit Tags
         </Button>
 
-        <dialog className="backdrop:bg-amber-500 flex justify-center items-center w-" ref={dialogRef} >
-          <EditTag closeModal={() => dialogRef.current?.close()} />
+        <dialog className="backdrop:bg-amber-500" ref={dialogRef}>
+          <EditTag closeModal={() =>  dialogRef.current?.close()} />
         </dialog>
       </div>
     </header>
