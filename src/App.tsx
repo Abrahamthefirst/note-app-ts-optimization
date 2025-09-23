d8ceeb30dee759ab9f6c892ee50230c809550f19
Abrahamthefirst <abrahamodusegun36@gmail.com>
2025-09-23 07:37:20 +0100

    added wrong import

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,52 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import RequireAuth from './components/RequireAuth';
 import Login from './features/auth/pages/Login';
 import Signup from './features/auth/pages/Signup';
 import ForgotPassword from './features/auth/pages/ForgotPassword';
 import ResetPassword from './features/auth/pages/ResetPassword';
-import { useRoleBasedRoutes } from './hooks/useRoleBasedRoutes';
 import RequestVerificationLink from './features/auth/pages/RequestVerificationLink';
+import DBLayout from './layout/DBLayout';
+import { Suspense, lazy } from 'react';
+
+const AdminDashboard = lazy(() => import('./pages/AdminDashboard'));
+const EditorDashboard = lazy(() => import('./pages/EditorDashboard'));
+const ViewerDashboard = lazy(() => import('./pages/ViewerDashboard'));
+const NoteList = lazy(() => import('./features/notes/components/NoteList'));
+const NewNote = lazy(() => import('./features/notes/pages/NewNote'));
 
 function App() {
-  const roleBasedRoutes = useRoleBasedRoutes();
+  // const roleBasedRoutes = useRoleBasedRoutes();
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<Signup />} />
-      <Route
-        path="email/verification-link/:token"
-        element={<RequestVerificationLink />}
-      />
-      <Route path="forgot-password" element={<ForgotPassword />} />
-      <Route path="reset-password/:token" element={<ResetPassword />} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {/* Public Routes - NO AUTH REQUIRED */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route
+          path="email/verification-link/:token"
+          element={<RequestVerificationLink />}
+        />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="reset-password/:token" element={<ResetPassword />} />
 
-      <Route element={<RequireAuth />}>{roleBasedRoutes}</Route>
+        {/* Protected Routes - AUTH REQUIRED */}
+        <Route element={<RequireAuth />}>
+          {/* Use static routing here to avoid the bug */}
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="editor" element={<EditorDashboard />} />
+          <Route path="viewer" element={<ViewerDashboard />} />
 
-      <Route path="*" element={<div>404 Page Not Found</div>} />
-      <Route />
-    </Routes>
+          <Route element={<DBLayout />}>
+            <Route path="/" element={<ViewerDashboard />} />
+            <Route path="directory/:directoryId" element={<NoteList />} />
+            <Route path="create-note/:directoryId" element={<NewNote />} />
+          </Route>
+        </Route>
+
+        {/* Catch-all for 404 pages */}
+        <Route path="*" element={<div>404 Page Not Found</div>} />
+      </Routes>
+    </Suspense>
   );
 }
 
